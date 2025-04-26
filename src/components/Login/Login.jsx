3c96f5e2846d41e45e0f6ef6597bb44d84fb2a56
faruqsoft <omarfaruqngdc@gmail.com>
2025-04-27 05:23:27 +0600

    google authentication added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { GoogleAuthProvider, signInWithPopup,signOut } from "firebase/auth";
+
+import {auth} from "../../Firebase/Firebase.init.js";
+
 
 const Login = () => {
+    const [user, setUser] = useState(null);
+
+
+    
+    const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
         console.log('Google signIn');
+
+        signInWithPopup(auth,provider)
+            .then((result) => {
+                console.log(result);
+                setUser(result.user);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+
+
+    }
+
+    const handleGoogleSignOut=()=>{
+       signOut(auth).then(() => {
+            console.log('Google signOut');
+            setUser(null);
+        }).catch((error) => {
+            console.log(error);
+        })
     }
     return (
         <div>
             <h1>Please login</h1>
-            <button onClick={()=>handleGoogleSignIn()}>Sign in with google</button>
+            <button onClick={handleGoogleSignIn}>Sign in with google</button>
+            <button onClick={handleGoogleSignOut}>Sign Out</button>
+
+            {
+                user && <div>
+                    <h3>{user.displayName}</h3>
+                    <p>{user.email}</p>
+                </div>
+            }
         </div>
     );
 };
